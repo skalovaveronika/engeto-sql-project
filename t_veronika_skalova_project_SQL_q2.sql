dc044e9bfd910b9e44b7dd58595c5cf94b023c07
Veronika Skalova <skalovaver@gmail.com>
2023-11-04 15:45:03 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q2.sql
+++ t_veronika_skalova_project_SQL_q2.sql
@@ -1,6 +1,18 @@
--- Q2 Base querry to extract data for milk and bread
-SELECT *
+-- Q2 Base querry to extract data for milk and bread, grouped by food_code and payroll year
+SELECT 
+	payroll_year, 
+	industry_code,
+	industry_name,
+	average_salary,
+	currency,
+	food_code,
+	food_type,
+	average_value,
+	food_quantity,
+	measure_unit
 FROM 
 	primary_final_v4
 WHERE 
-	food_code IN ('114201', '111301') 
+	food_code IN ('114201', '111301')  
+GROUP BY 
+	food_code, payroll_year;
