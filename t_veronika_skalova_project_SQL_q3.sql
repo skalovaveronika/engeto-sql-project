cd9340b5ccf671ac71a53a118978ee15b4595624
Veronika Skalova <skalovaver@gmail.com>
2023-11-04 18:32:09 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q3.sql
+++ t_veronika_skalova_project_SQL_q3.sql
@@ -9,3 +9,25 @@ FROM
 	primary_final_v4
 GROUP BY 
 	food_code, food_type, price_year;
+
+-- Q3 in progress
+WITH food_price_changes AS (
+	SELECT 
+		price_year,
+		food_code,
+		food_type,
+		average_value,
+		LAG(average_value) OVER (PARTITION BY food_code, food_type ORDER BY price_year) AS previous_year_value
+	FROM 
+		primary_final_v4
+	GROUP BY 
+		food_code, food_type, price_year
+)
+SELECT 
+	food_code,
+	food_type,
+	(average_value - previous_year_value) / average_value * 100
+FROM 
+	food_price_changes
+GROUP BY
+	food_code, food_type
