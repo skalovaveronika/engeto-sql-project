f35b79d150174ad03aa9d6c798d5194bcf9d3c9e
Veronika Skalova <skalovaver@gmail.com>
2023-11-04 15:25:22 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q1.sql
+++ t_veronika_skalova_project_SQL_q1.sql
@@ -1,4 +1,4 @@
--- Q1 base querry
+-- Q1 Base querry
 SELECT
 	payroll_year,
 	industry_code,
@@ -41,3 +41,31 @@ FROM
 	salary_changes_q1
 ORDER BY 
 	industry_code, payroll_year;
+
+/* Used the same CTE 'salary_changes_q1' to get the overall % change in average salary by industry across the years, which perhaps answers the original question a little better. 
+   The results are grouped by industry code and name, and the industries are ordered by the overall salary change percentage in ascending order.
+*/
+WITH salary_changes_q1 AS (
+	SELECT
+		payroll_year,
+		industry_code,
+		industry_name,
+		average_salary,
+		LAG(average_salary) OVER (PARTITION BY industry_code ORDER BY payroll_year) AS previous_yr_avg_salary
+	FROM 
+		primary_final_v4 pf
+	GROUP BY 
+		payroll_year, industry_code
+)
+SELECT
+	industry_code,
+	industry_name,
+	ROUND(SUM(average_salary - previous_yr_avg_salary) / SUM(previous_yr_avg_salary) * 100, 2) AS overall_salary_change_percentage
+FROM 
+	salary_changes_q1
+GROUP BY 
+	industry_code, industry_name
+ORDER BY 
+	overall_salary_change_percentage ASC;
+
+
