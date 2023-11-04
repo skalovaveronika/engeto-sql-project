b2fc718635ac2d27be939d85a754c2e0910efcfd
Veronika Skalova <skalovaver@gmail.com>
2023-11-04 18:18:48 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q2.sql
+++ t_veronika_skalova_project_SQL_q2.sql
@@ -17,7 +17,10 @@ WHERE
 GROUP BY 
 	food_code, payroll_year;
 	
--- Q2 in progress
+/* The querry calculates the purchasing power for 'Bread' and 'Milk' by analyzing the average salary and food prices over time.
+Payroll_years subquery was joined with bread_milk_avg_salary to get results for the the first and last comparable periods (years).
+The final output is grouped by payroll year, food code, and food type.
+*/
 WITH bread_milk_avg_salary AS (
 	SELECT 
 		payroll_year, 
@@ -34,18 +37,33 @@ WITH bread_milk_avg_salary AS (
 		food_code IN ('114201', '111301') 
 	GROUP BY 
 		food_code, payroll_year
+),
+	payroll_years AS ( -- subquerry to calculate min and max payroll years
+		SELECT 
+			MIN(payroll_year) AS min_year,
+			MAX(payroll_year) AS max_year
+		FROM 
+			bread_milk_avg_salary
 ) 
 SELECT
-	payroll_year,
-	AVG(average_salary) AS average_salary_across_industries,
-	currency,
-	food_code,
-	food_type,
-	average_value AS average_price,
-	currency,
-	ROUND((AVG(average_salary) / average_value), 2) AS food_purchasing_power,
-	measure_unit
+	bmas.payroll_year,
+	AVG(bmas.average_salary) AS average_salary_across_industries,
+	bmas.currency,
+	bmas.food_code,
+	bmas.food_type,
+	bmas.average_value AS average_price,
+	bmas.currency,
+	ROUND((AVG(bmas.average_salary) / bmas.average_value), 2) AS food_purchasing_power,
+	bmas.measure_unit
 FROM 
-	bread_milk_avg_salary 
-GROUP BY 
-	payroll_year, food_code;
+	bread_milk_avg_salary AS bmas
+JOIN
+	payroll_years AS py 
+ON 
+	1=1 -- join condition for cross join
+WHERE 
+	bmas.payroll_year = py.min_year OR bmas.payroll_year = py.max_year
+GROUP BY
+	bmas.payroll_year, bmas.food_code, bmas.food_type, bmas.currency, bmas.average_value, bmas.measure_unit
+ORDER BY 
+	bmas.food_code, bmas.payroll_year;
