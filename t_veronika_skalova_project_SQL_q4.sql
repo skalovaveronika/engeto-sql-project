05935afe5538f8e2608ea248d814de822b73c69e
Veronika Skalova <skalovaver@gmail.com>
2023-11-05 11:19:05 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q4.sql
+++ t_veronika_skalova_project_SQL_q4.sql
@@ -1,8 +1,9 @@
+-- Q4 Base querry to analyze the average salary and food prices over multiple years.
 SELECT 
 	payroll_year,
 	price_year,
-	ROUND(average_salary,2) AS avg_salary,
-	ROUND(average_value,2) AS avg_food_price
+	ROUND(AVG(average_salary),2) AS avg_salary,
+	ROUND(AVG(average_value),2) AS avg_food_price
 FROM 
 	primary_final_v4
 GROUP BY 
