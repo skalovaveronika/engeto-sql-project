b1086f6105a765a2e767d956d2e381572f6cf64b
Veronika Skalova <skalovaver@gmail.com>
2023-11-05 12:45:32 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q4.sql
+++ t_veronika_skalova_project_SQL_q4.sql
@@ -36,3 +36,41 @@ ON
 	Y.payroll_year = P.payroll_year + 1 -- left join on payroll YEAR
 ORDER BY 
 	Y.payroll_year;
+	
+/* Q4 Final querry to calculate yearly averages of salaries and food prices. 
+The result shows years where the percentual change in food prices is more than 10% higher than the percentage change in salaries.
+*/
+WITH yearly_averages AS (
+	SELECT 
+			payroll_year,
+			ROUND(AVG(average_salary),2) AS avg_salary,
+			price_year,
+			ROUND(AVG(average_value),2) AS avg_food_price
+	FROM 
+		primary_final_v4
+	GROUP BY 
+		payroll_year, price_year 
+)
+, percentual_changes AS ( 
+SELECT 
+	Y.payroll_year,
+	Y.price_year,
+	Y.avg_salary,
+	ROUND(((Y.avg_salary - LAG(Y.avg_salary) OVER (ORDER BY Y.payroll_year)) / LAG(Y.avg_salary) OVER (ORDER BY Y.payroll_year)) * 100, 2) AS salary_change_percent,
+	Y.avg_food_price,
+	ROUND(((Y.avg_food_price - LAG(Y.avg_food_price) OVER (ORDER BY Y.payroll_year)) / LAG(Y.avg_food_price) OVER (ORDER BY Y.payroll_year)) * 100, 2) AS food_change_percent
+FROM 
+	yearly_averages Y
+LEFT JOIN
+	yearly_averages P 
+ON 
+	Y.payroll_year = P.payroll_year + 1 -- left join on payroll YEAR
+) 
+SELECT 
+	*
+FROM 
+	percentual_changes
+WHERE 
+	food_change_percent - salary_change_percent > 10
+ORDER BY 
+	payroll_year;
