d9a034daaf53d16483266d53e866f7b51777a9b8
Veronika Skalova <skalovaver@gmail.com>
2023-11-05 12:03:44 +0100

    Updated file posted

--- t_veronika_skalova_project_SQL_q2.sql
+++ t_veronika_skalova_project_SQL_q2.sql
@@ -1,13 +1,10 @@
--- Q2 Base querry to extract data for milk and bread, grouped by food_code and payroll year
+-- Q2 Base querry to extract data for milk and bread, grouped by food_code and payroll year. Includes data for average salary across industries over time. 
 SELECT 
 	payroll_year, 
-	industry_code,
-	industry_name,
-	average_salary,
+	ROUND(AVG(average_salary),2) AS average_salary,
 	currency,
-	food_code,
 	food_type,
-	average_value,
+	ROUND(AVG(average_value),2) AS average_price,
 	food_quantity,
 	measure_unit
 FROM 
@@ -24,36 +21,34 @@ The final output is grouped by payroll year, food code, and food type.
 WITH bread_milk_avg_salary AS (
 	SELECT 
 		payroll_year, 
-		food_code,
-		food_type,
-		average_salary,
+ 		ROUND(AVG(average_salary), 2) AS average_salary,
 		currency,
-		average_value,
+		food_type,
+		ROUND(AVG(average_value), 2) AS average_price,
 		food_quantity,
 		measure_unit
 	FROM 
 		primary_final_v4
 	WHERE 
-		food_code IN ('114201', '111301') 
+		food_code IN ('114201', '111301')  
 	GROUP BY 
-		food_code, payroll_year
+		payroll_year, food_type, currency, food_quantity, measure_unit
 ),
-	payroll_years AS ( -- subquerry to calculate min and max payroll years
-		SELECT 
-			MIN(payroll_year) AS min_year,
-			MAX(payroll_year) AS max_year
-		FROM 
-			bread_milk_avg_salary
+payroll_years AS (
+	SELECT 
+		MIN(payroll_year) AS min_year,
+		MAX(payroll_year) AS max_year
+	FROM 
+		bread_milk_avg_salary
 ) 
 SELECT
 	bmas.payroll_year,
-	AVG(bmas.average_salary) AS average_salary_across_industries,
+	bmas.average_salary AS average_salary,
 	bmas.currency,
-	bmas.food_code,
 	bmas.food_type,
-	bmas.average_value AS average_price,
+	bmas.average_price,
 	bmas.currency,
-	ROUND((AVG(bmas.average_salary) / bmas.average_value), 2) AS food_purchasing_power,
+	ROUND((AVG(bmas.average_salary) / bmas.average_price), 2) AS food_purchasing_power,
 	bmas.measure_unit
 FROM 
 	bread_milk_avg_salary AS bmas
@@ -64,6 +59,6 @@ ON
 WHERE 
 	bmas.payroll_year = py.min_year OR bmas.payroll_year = py.max_year
 GROUP BY
-	bmas.payroll_year, bmas.food_code, bmas.food_type, bmas.currency, bmas.average_value, bmas.measure_unit
+	bmas.payroll_year, bmas.food_type, bmas.currency, bmas.average_price, bmas.measure_unit
 ORDER BY 
-	bmas.food_code, bmas.payroll_year;
+	bmas.food_type, bmas.payroll_year;
